50ed85ad4fe3505121ff53b8a5954f4440756a0c
agoenks29D <agungmasda29@gmail.com>
2022-01-29 03:37:51 +0700

    added database config file

---
+++ config/database.js
@@ -0,0 +1,13 @@
+module.exports.host = 'localhost';
+
+module.exports.port = '3306';
+
+module.exports.username = 'root';
+
+module.exports.password = '';
+
+module.exports.database = 'nodejs-app-simple';
+
+module.exports.dbdriver = 'mysql';
+
+module.exports.debuging = false;
